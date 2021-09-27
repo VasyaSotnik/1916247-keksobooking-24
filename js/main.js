800961e52490219505fbcbb83747476b4fcc9c5c
Vasiliy Sotnikov <gangasboy67@gmail.com>
2021-09-27 05:51:01 +0500

    задание

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const getRandom = (min, max) => {
+
+  const random = (min < 0) ? console.log('Пожалуйста, введите положительное значение!')
+  : (max <= min) ? console.log('Пожалуйста, введите значение больше значения "ОТ"')
+  : Math.random() * (max - min + 1);
+  return random;
+}; // Число с плавающей точкой
+
+
+const getRandom = (min, max) => {
+  min = Math.round(min);
+  max = Math.round(max);
+  const random = (min < 0) ? console.log('Пожалуйста, введите положительное значение!')
+  : (max <= min) ? console.log('Пожалуйста, введите значение больше значения "ОТ"')
+  : Math.round(Math.random() * (max - min + 1));
+  return random;
+}; // - Вызов целого числа
+
+
+/*
+
+https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random - отсюда подсмотрел метод math.Random
+
+
+*/
